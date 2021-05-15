f668557069751000c9173c65b77838300e3cd969
Cihat Solak <cihatsolak@hotmail.com>
2021-05-15 14:23:48 +0300

    Initialize project

---
+++ RabbitMQPublishExcel/PublishExcel.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PublishExcel.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
